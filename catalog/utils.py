45277a38be639cf146d78e4617dcefd8fad02074
Vlad4Per <pv01042004@gmail.com>
2025-01-19 23:34:26 +0500

    update search

--- catalog/utils.py
+++ catalog/utils.py
@@ -1,15 +1,23 @@
+from django.contrib.postgres.search import SearchVector
+
 from catalog.models import Product
 from django.db.models import Q
+
 
 def q_search(query):
 
     if query.isdigit() and len(query) <= 5:
         return Product.objects.filter(id=int(query))
 
-    tokens = list(filter(lambda x:len(x)>2,query.split()))
+    return Product.objects.annotate(search=SearchVector(
+        "name", "description"
+    )).filter(search=query)
 
-    qs = Q()
-    for token in tokens:
-        qs |= Q(description__icontains=token)
-        qs |= Q(name__icontains=token)
-    return Product.objects.filter(qs)
+
+    # tokens = list(filter(lambda x:len(x)>2,query.split()))
+    #
+    # qs = Q()
+    # for token in tokens:
+    #     qs |= Q(description__icontains=token)
+    #     qs |= Q(name__icontains=token)
+    # return Product.objects.filter(qs)
