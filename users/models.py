0de03820286b10a4c4e839bea26f1c0afd1ebb50
Vlad4Per <pv01042004@gmail.com>
2025-01-20 12:00:43 +0500

    login form in users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', null=True, blank=True)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.username
