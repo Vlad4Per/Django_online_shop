4d6a194d2155b8a3815896b4e5a8003076257728
Vlad4Per <pv01042004@gmail.com>
2025-01-20 21:27:08 +0500

    sign up form and controller

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,30 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django import forms
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField()
-    password = forms.CharField()
+    # username = forms.CharField()
+    # password = forms.CharField()
     class Meta:
         model = User
+
+
+class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2'
+        )
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegisterForm
 
 
 def login(request):
@@ -26,8 +26,18 @@ def login(request):
 
 
 def signup(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
     context = {
-
+        'title':'signup',
+        'form':form,
     }
     return render(request, 'users/signup.html', context=context)
 
@@ -39,4 +49,5 @@ def profile(request):
     return render(request, 'users/profile.html', context=context)
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('user:login'))
