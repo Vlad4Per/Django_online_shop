80c6fd549dc3dcb81d55b6f830f78e2d11cc9198
Vlad4Per <pv01042004@gmail.com>
2025-01-21 17:45:00 +0500

    cart view added and templates changes

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.db.models import Sum
 
-# Create your models here.
+from catalog.models import Product
+from users.models import User
+
+class CartQueryset(models.QuerySet):
+    def total_carts_cost(self):
+        return sum(cart.total_cost() for cart in self)
+
+    def total_quantity(self):
+        if self:
+            return sum(cart.quantity for cart in self)
+        return 0
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    session_key = models.CharField(max_length=40, blank=True, null=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'cart'
+        ordering = ['-created_timestamp']
+
+    objects = CartQueryset.as_manager()
+    def total_cost(self):
+        return round(self.quantity * self.product.sell_cost(), 2)
+
+    def __str__(self):
+        return f'{self.user.username} - Товар: {self.product.name} в количестве {self.quantity}'
--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-def cart_add(request, product_id):
+from cart.models import Cart
+from catalog.models import Product
+
+
+def cart_add(request, product_slug):
+    prod = Product.objects.get(slug=product_slug)
+    if request.user.is_authenticated:
+        cart = Cart.objects.filter(user=request.user, product=prod)
+
+        if cart.exists():
+            cart = cart.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user, product=prod, quantity=1)
+    return redirect(request.META.get('HTTP_REFERER'))
+
+def cart_change(request, product_slug):
     ...
 
-def cart_change(request, product_id):
-    ...
-
-def cart_remove(request, product_id):
+def cart_remove(request, product_slug):
     ...
 
